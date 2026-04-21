db02ab7edf6aa3241fcc5b681bc6755feba9f103
LiveTrack <livetrack@local>
2026-04-21 20:06:29 +0200

    chore: suppression secrets.template.js (obsolete depuis printf CI)

--- js/secrets.template.js
+++
@@ -1,9 +0,0 @@
-/**
- * LiveTrack Rally - Secrets Template
- * This file is used by GitHub Actions to inject sensitive parameters.
- */
-const SECRETS = {
-    TELEGRAM_ADMIN_TOKEN: "${TELEGRAM_ADMIN_BOT_TOKEN}",
-    TELEGRAM_ADMIN_CHAT_ID: "${TELEGRAM_ADMIN_CHAT_ID}",
-    GDRIVE_WEBHOOK_URL: "${GDRIVE_WEBHOOK_URL}"
-};
